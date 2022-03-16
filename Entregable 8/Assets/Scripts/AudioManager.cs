e519eea619cd7afda05c8026ae5e43f5dd037201
Rowen Lumanlan <91453650+HASHlRA@users.noreply.github.com>
2022-03-16 23:48:22 +0100

    Not done

---
+++ Entregable 8/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AudioManager : MonoBehaviour
+{
+    private AudioSource audioSource
+    public GameObject title;
+    private int audioIndex;
+    public AudioClip[] audioClips;
+    public string[] audioNames;
+    private int audioIndex = 0;
+
+
+    void Start()
+    {
+        // Adquiere el AudioSource
+        audioSource = GetComponent<AudioSource>();
+
+        // Introduce el clip
+        audioSource.clip = audioClips[audioIndex];
+
+        // Muestra el título
+        ShowAudioName();
+    }
+    
+    // Pasa a la siguiente canción y la reproduce
+    public void NextAudio()
+    {
+        // Aumenta el index
+        audioIndex++;
+
+        // Si el Index es mayor o igual al númera de clips en total
+        if (audioIndex >= audioClips.Lenght)
+        {
+            // Se reinicia a 0
+            audioIndex = 0;
+        }
+
+        // Reproduce la canción
+        StartAudio();
+    }
+
+    // Pasa a la anterior canción y la reproduce
+    public void PreviousAudio()
+    {
+        // Disminuye el index
+        audioIndex--;
+
+        // Si el index es menor 0
+        if (audioIndex < 0)
+        {
+            // Número total de clips
+            audioIndex = audioClips.Lenght - 1;
+        }
+
+        // Reproduce la canción
+        StartAudio();
+    }
+
+    // Empieza y reanuda la canción
+    public void PlayAudio()
+    {
+        // Si el audio está en pausa
+        if (!audioSource.isPlaying)
+        {
+            // Reproduce la canción
+            audioSource.Play();
+        }
+    }
+
+    // Pausa la canción
+    public void PauseAudio()
+    {
+        audioSource.Pause();
+    }
+
+    // Reproduce una canción aleatoria
+    public void RandomAudio()
+    {
+        // Obtiene el valor aleatorio de las canciones
+        int randomIndex = Random.Range(0, audioClips.Lenght);
+
+        // Index recibe el valor aleatorio
+        audioIndex = randomIndex;
+
+        // Reroduce el audio
+        StartAudio();
+    }
+
+    // Muestra el título de la canción
+    private void ShowAudioName()
+    {
+        // Cambia el título dependiendo de la canción
+        title.GetComponent<TextMeshProUGUI>().text = audioNames[audioIndex];
+    }
+
+    // Reproduce la canción
+    public void StartAudio()
+    {
+        // Inserta el clip correspondiente al Index
+        audioSource.clip = audioClips[audioIndex];
+
+        // Detiene y reproduce la canción
+        StopAudio();
+        PlayAudio();
+
+        // Muestra el nombre de la canción
+        ShowAudioName();
+    }
+}
